9641197220a520640a9413bd1520eb129f778570
ERROR244 <khalilsohail24@gmail.com>
2024-05-23 16:51:21 +0100

    (updated ft_split_str() | working on echo get_final_line() needed by expand_variable()

--- src/parsing_utils7.c
+++ src/parsing_utils7.c
@@ -6,25 +6,11 @@
 /*   By: ksohail- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 20:57:54 by ksohail-          #+#    #+#             */
-/*   Updated: 2024/05/23 16:34:34 by ksohail-         ###   ########.fr       */
+/*   Updated: 2024/05/23 16:50:54 by ksohail-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
-#include "../Libft/libft.h"
-
-int cheak_num(char *s)
-{
-	char *start;
-
-	start = s;
-	s++;
-	while (ft_isalpha(*s) == 1)
-		s++;
-	if (ft_isdigit(*s) == 1)
-		return (s - start);
-	return (0);
-}
 
 static int	count(char *s1)
 {
@@ -35,12 +21,6 @@ static int	count(char *s1)
 	in_word = 0;
 	while (s1 && *s1)
 	{
-		if (*s1 == '$' && cheak_num(s1) != 0)
-		{
-			while (ft_isdigit(*s1) == 0)
-				s1++;
-			s1++;
-		}
 		if (*s1 == '$' && ft_isalpha(s1[1]) == 1)
         {
             s1++;
@@ -48,7 +28,6 @@ static int	count(char *s1)
             {
                 while (*s1 && ft_isalpha(s1[0]) == 1)
 			    {
-					// printf("----->:$%c:\n", *s1);
                     s1++;
                 }
 			    in_word = 0;
@@ -57,18 +36,12 @@ static int	count(char *s1)
 		else if (in_word == 0)
 		{
 		    count++;
-			// printf(":");
             while (*s1 && (*s1 != '$' || (ft_isalnum(s1[0]) == 1) || !s1[1]))
 		    {
-				// printf("%c", *s1);
 				if (s1[0] == '$' && s1[1] == '$')
-				{
 					s1++;
-					// printf("%c", *s1);
-				}
                 s1++;
             }
-			// printf(":\n");
 		    in_word = 1;
 		}
         else
@@ -81,26 +54,17 @@ static char	*dup_size(char *s, size_t n)
 {
 	char	*dup;
 	size_t	i;
-	size_t	j;
 
 	if (!s)
 		return (NULL);
 	dup = (char *)malloc((n + 1) * sizeof(char));
 	if (dup != NULL)
 	{
-		j = 0;
 		i = 0;
-		while (j < n)
+		while (i < n)
 		{
-			if (s[i] == '$' && cheak_num(s) != 0)
-			{
-				while (ft_isdigit(s[i]) == 0)
-					i++;
-				i++;
-			}
-			dup[j] = s[i];
+			dup[i] = s[i];
 			i++;
-			j++;
 		}
 		dup[n] = '\0';
 	}
@@ -120,35 +84,22 @@ static void	ft_free(char **ptr, int i)
 static char	**split(char *s1, int i, char **ptr)
 {
 	char	*start;
-	int		flag;
 
 	while (*s1)
 	{
-		
 		if (*s1 != '$' || ft_isalnum(s1[1]) == 0)
 		{
-			flag = 0;
+
 			start = s1;
             while (*s1)
             {
-				if (*s1 == '$' && cheak_num(s1) != 0)
-				{
-					flag += cheak_num(s1) + 1;
-					while (ft_isdigit(*s1) == 0)
-						s1++;
-					s1++;
-					printf(":%c:\n", *s1);
-				}
-                if (*s1 == '$' && ft_isalnum(s1[1]) == 0)
+				if (*s1 == '$' && ft_isalnum(s1[1]) == 0)
                     s1++;
 				else if (*s1 == '$')
 					break ;
-				printf("_________________________>%c \n", *s1);
 				s1++;
             }
-			printf("flag----->%d \n", flag);
-			printf("%ld \n", s1 - start - flag);
-			ptr[i] = dup_size(start, s1 - start - flag);
+			ptr[i] = dup_size(start, s1 - start);
 			if (ptr[i] == NULL)
 			{
 				ft_free(ptr, i);
@@ -159,7 +110,7 @@ static char	**split(char *s1, int i, char **ptr)
 		else
         {
             s1++;
-            while (*s1 && ft_isalpha(*s1) == 1)
+            while (*s1 && ft_isalnum(*s1) == 1)
                 s1++;
         }
 	}
